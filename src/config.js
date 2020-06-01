af294efd947f740824bd1c68cd51e7e701f1f80a
wave <wave98688@gmail.com>
2020-06-01 10:31:38 +0800

    配置修改

--- src/config.js
+++ src/config.js
@@ -7,10 +7,10 @@
 export const IS_DEV = process.env.NODE_ENV === 'development';
 //燃烧地址的公钥
 export const API_BURNING_ADDRESS_PUB = '000000000000000000000000000000000000000000000000000000000000000000';
-//chainId、assetId、prefix
-export const MAIN_INFO = {chainId: 4, assetId: 1, prefix: 'TNVT'};
+//ChainId和资产ID
+export const MAIN_INFO = {chainId: 3, assetId: 1, prefix: 'NVT'};
 //跨链资产信息 chainId、assetId
-export const CROSS_INFO = {chainId: 2, assetId: 1};
+export const CROSS_INFO = {chainId: 1, assetId: 1};
 //正式、测试网络的api
 const url = localStorage.hasOwnProperty("url") && localStorage.getItem('url') !== 'undefined' ? JSON.parse(localStorage.getItem("url")).urls : '/api';
 let API_URL = url;
@@ -46,28 +46,7 @@ export const explorerData = [
     selection: false,
     isDelete: false
   },
-  {
-    name: '456',
-    chainId: MAIN_INFO.chainId,
-    assetId: MAIN_INFO.assetId,
-    decimals: 8,
-    chainName: 'nerve',
-    urls: 'http://seede.nuls.io:17003',
-    delay: '10ms',
-    selection: false,
-    isDelete: false
-  },
-  {
-    name: '789',
-    chainId: MAIN_INFO.chainId,
-    assetId: MAIN_INFO.assetId,
-    decimals: 8,
-    chainName: 'nerve',
-    urls: 'http://192.168.1.141:17003',
-    delay: '10ms',
-    selection: false,
-    isDelete: false
-  },
 ];
 //默认浏览器地址（跳转地址）
 export const explorerUrl = IS_DEV ? 'http://beta.nervecan.nervedex.com/' : 'https://nervecan.nervedex.com/';
+
